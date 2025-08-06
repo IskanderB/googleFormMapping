cb4608376216ccdb1d0b4e75c76a4a0c2ae8674b
alexanderkhurtin <axurtin.rep@gmail.com>
2025-08-06 23:02:50 +0300

    Add sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,7 +45,8 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            // Отправит все неигнорируемые исключения в Sentry
+            Integration::captureUnhandledException($e);
         });
     }
 }
